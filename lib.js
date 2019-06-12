ad7e24f80b086145706cd0745ce91c4b66b40d32
david <davidqhr@gmail.com>
2019-06-12 18:50:06 +0800

    update ganache-core

--- lib.js
+++ lib.js
@@ -1,6 +1,6 @@
 // make sourcemaps work!
-require('source-map-support/register')
+require("source-map-support/register");
 
-module.exports = require("ganache-core/public-exports.js");
-module.exports.version = require("ganache-core/package.json").version;
-module.exports.to = require("ganache-core/lib/utils/to");
+module.exports = require("@davidqhr/ganache-core/public-exports.js");
+module.exports.version = require("@davidqhr/ganache-core/package.json").version;
+module.exports.to = require("@davidqhr/ganache-core/lib/utils/to");
